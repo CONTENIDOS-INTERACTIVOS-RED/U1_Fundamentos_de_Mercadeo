85ae72d521ae955132353c7343f55f3088f6970e
Francisco Lizcano <flizcanor@icloud.com>
2025-12-17 20:20:01 -0500

    ajustes

--- src/views/curso/Tema1.vue
+++ src/views/curso/Tema1.vue
@@ -510,7 +510,7 @@
               a(href="https://elibro.net/es/lc/tecnologicadeloriente/titulos/274611" target="_blank" rel="noopener noreferrer") De Castro Emilio Cerdá, F. J. A. L. M. (s/f). Las energías renovables en el ámbito internacional. Revistasice.com. 
 
             p.d-flex.my-4
-              img.me-3(src='@/assets/template/book.svg' :style="{'max-width':'16px'}")
+              img.me-3(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
               a(href="https://www.un.org/spanish/esa/sustdev/agenda21/riodeclaration.htm" target="_blank" rel="noopener noreferrer") De Janeiro, R. (1992). La declaración del Río sobre el Medio Ambiente y el Desarrollo. Río de Janeiro República Federativa del Brasil., aprobada en Estocolmo el, 16.
 
             p.d-flex.my-4
